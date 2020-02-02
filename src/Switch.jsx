93c0dbfca5ce0c3e12a6c9d80d3fdd07fb2de859
Katie George <katiegeorge94@gmail.com>
2020-02-02 13:36:05 -0700

    add key press

--- src/Switch.jsx
+++ src/Switch.jsx
@@ -2,9 +2,15 @@ import React from 'react';
 import './styles/main.scss';
 
 function Switch({isChecked, toggleValue}) {
+    const handleKeyPress = e => {
+        e.stopPropagation();
+        if (e.key === 'Enter' || e.keyCode === 13) {
+            toggleValue(e);
+        }
+    };
 
     return (
-        <label className="switch">
+        <label className="switch" tabIndex="0" onKeyDown={handleKeyPress}>
             <input
                 aria-checked={isChecked}
                 type="checkbox"
